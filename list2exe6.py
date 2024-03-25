e168178eaf3c1360aa5ee4f531d22eea643de6cb
IagoRibeiro240 <160283535+IagoRibeiro240@users.noreply.github.com>
2024-03-25 19:51:18 -0300

    Lista de Exercício 2

---
+++ list2exe6.py
@@ -0,0 +1,14 @@
+qtd_linhas = 20
+qtd_colunas = 20
+
+linha = 1 
+while linha <= qtd_linhas:
+    
+    coluna = 1
+    while coluna <= qtd_colunas:
+    
+        print(f'{linha=} {coluna=}')
+        coluna += 1
+    linha += 1
+  
+print("Acabou")
